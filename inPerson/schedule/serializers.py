efa9c5536f1852ace8332fa4f5e096a67973840e
atgao <alice.gao@princeton.edu>
2019-03-22 08:48:25 -0400

    added serializer for classes

---
+++ inPerson/schedule/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Classes
+
+
+class Classes(serializers.ModelSerializer):
+    class Meta:
+        model = Classes
+        fields = ("code", "catalog_number". "section")
