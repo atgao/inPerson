5a29ee8602fe1bf391da089a99818eb5da280241
atgao <alice.gao@princeton.edu>
2019-03-26 10:11:04 -0400

    edited managers.py for admin

--- inPerson/users/models.py
+++ inPerson/users/models.py
@@ -6,10 +6,10 @@ from django.conf import settings
 class User(AbstractUser):
     #netid = models.CharField(max_length=15, unique=True, default=None, null=True)
     class_year = models.IntegerField(blank=False, default=2022)
-    #email = models.EmailField(_('email address'), unique=True)
+    email = models.CharField(max_length=15, default=None, null=True)
 
     #
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'class_year']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'class_year', 'email']
 
     def __str__(self):
         return "{}".format(self.username)
