c8d29ffba7e8096d7b28dd22ff2b3fb68981441b
atgao <alice.gao@princeton.edu>
2019-03-26 10:40:08 -0400

    trying to get username/netid prompt for regular user

--- inPerson/users/models.py
+++ inPerson/users/models.py
@@ -3,13 +3,17 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
+from .managers import CustomUserManager
+
 class User(AbstractUser):
-    #netid = models.CharField(max_length=15, unique=True, default=None, null=True)
+    netid = models.CharField(max_length=15, unique=True, default=None, null=True)
     class_year = models.IntegerField(blank=False, default=2022)
     email = models.CharField(max_length=15, default=None, null=True)
 
-    #
+    USERNAME_FIELD = 'netid'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'class_year', 'email']
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return "{}".format(self.username)
