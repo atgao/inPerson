07f15da30e4a121f58a08fe916e202e56df16e13
atgao <alice.gao@princeton.edu>
2019-03-26 22:00:23 -0400

    created schedule model

--- inPerson/schedule/models.py
+++ inPerson/schedule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from datetime import datetime
 from django.contrib.postgres.fields import ArrayField
 
@@ -22,11 +23,20 @@ class Classes(models.Model):
 # time every week, such as a sports practice or class. note that classes are
 # TRANFORMED into a recurrent event
 class RecurrentEvent(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, blank=False, null=False)
     start_time = models.TimeField(blank=False, null=False)
     end_time = models.TimeField(blank=False, null=False)
     days = ArrayField(models.CharField(max_length=10,blank=False, null=False))
     location = models.CharField(max_length=200, blank=True, null=True)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} {} {}-{}".format(self.title, self.location, self.start_time, self.end_time)
 
 class Schedule(models.Model):
-    
+    semester = models.CharField(max_length=5, blank=False, null=False) #F18, S19, etc
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}".format(self.semester)
