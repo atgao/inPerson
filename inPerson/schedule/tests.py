ad97843a8f957732ff5030e2ce949274a88ed96e
atgao <alice.gao@princeton.edu>
2019-03-26 23:28:45 -0400

    working on tests

--- inPerson/schedule/tests.py
+++ inPerson/schedule/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from .models import RecurrentEvent
+from .models import Schedule
 
 # Create your tests here.
+class RecurrentEventsTest(APITestCase):
+    client = APIClient()
+
+    @classmethod
+    def setup_test_data():
+        #set up a user
+        user = User.objects.create()
+
+    def create_recurrentevent(title="", start_time="", end_time="", days="", location=""):
+        if title != "" and artist != "":
+            Songs.objects.create(title=title, artist=artist)
