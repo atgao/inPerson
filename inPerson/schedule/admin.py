eadea2ba539cebae9960950f217bc0023e4b63e5
atgao <alice.gao@princeton.edu>
2019-03-30 14:13:06 -0400

    added test for scheduling

--- inPerson/schedule/admin.py
+++ inPerson/schedule/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Section
+from .models import RecurrentEvent
 
 admin.site.register(Section)
+admin.site.register(RecurrentEvent)
 # Register your models here.
--- inPerson/schedule/tests.py
+++ inPerson/schedule/tests.py
@@ -2,19 +2,29 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
+
 from .models import RecurrentEvent
 from .models import Schedule
-from users.models import User
 
-# Create your tests here.
-# class RecurrentEventsTest(APITestCase):
-#     client = APIClient()
-#
-#     @classmethod
-#     def setup_test_data():
-#         #set up a user
-#         user = User.objects.create(first_name="John", last_name="Doe", class_year="test@example.com")
-#
-#     def create_recurrentevent(title="", start_time="", end_time="", days="", location=""):
-#         if title != "" and artist != "":
-#             Songs.objects.create(title=title, artist=artist)
+from users.models import User
+from django.contrib.auth import get_user_model
+
+
+
+class ScheduleTest(APITestCase):
+    """
+    Test that a schedule belongs to the correct user
+    """
+
+    def setUp(self):
+        # create a temporary user
+        User = get_user_model()
+        User.objects.create_user(first_name="Rob", last_name="Sedgewick", username="rsedgewick", class_year=2022, password="password")
+        user1 = User.objects.get(username="rsedgewick")
+        Schedule.objects.create(semester="S19", owner=user1)
+
+    def test_schedule_to_a_user(self):
+        user1 = User.objects.get(username="rsedgewick")
+        a_schedule = Schedule.objects.get(owner=user1)
+        self.assertEqual(a_schedule.semester, "S19")
+        self.assertEqual(a_schedule.owner.username, "rsedgewick")
