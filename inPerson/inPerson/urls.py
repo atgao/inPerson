d2104d91233656dbddb56a30082636f128af1bf3
atgao <alice.gao@princeton.edu>
2019-04-06 08:38:17 -0400

    installed django friendship library

--- inPerson/inPerson/urls.py
+++ inPerson/inPerson/urls.py
@@ -20,8 +20,10 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('friendship/', include('friendship.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('events/', include('schedule.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+
 ]
