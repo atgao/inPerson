e1f70d57f20e76bd6ac0a49176946729092469db
atgao <alice.gao@princeton.edu>
2019-04-11 08:42:40 -0400

    trying to fix settings

--- inPerson/inPerson/settings.py
+++ inPerson/inPerson/settings.py
@@ -180,5 +180,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 django_heroku.settings(locals())
