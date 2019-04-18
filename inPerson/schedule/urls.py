2e0df384c1993adda0cf0bc0f34954e3a45adecc
atgao <alice.gao@princeton.edu>
2019-04-17 20:19:30 -0400

    added login required decorator

--- inPerson/schedule/urls.py
+++ inPerson/schedule/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
-    path('user/', views.ListCreateRecurrentEventsView.as_view(),
+    path('user/', login_required(views.ListCreateRecurrentEventsView.as_view()),
         name='get-schedule-add-event'),
-    path('<int:pk>/', views.RecurrentEventsDetailView.as_view(),
+    path('<int:pk>/', login_required(views.RecurrentEventsDetailView.as_view()),
         name ='create-update-delete-recurrent-event'),
 
 
