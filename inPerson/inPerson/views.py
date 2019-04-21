86c8407e354369022049d84364fd2d8ba3f3bf2e
atgao <alice.gao@princeton.edu>
2019-04-21 15:39:13 -0400

    fixed issue with anonymous user

--- inPerson/inPerson/views.py
+++ inPerson/inPerson/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from schedule.models import Schedule
 from schedule.serializers import SchedulesSerializer
 
-CURRENT_TERM = "S2019" 
+CURRENT_TERM = "S2019"
 
 def menu_view(request):
+    if request.user.is_anonymous:
+        return render(request, 'home.html')
     schedule = Schedule.objects.filter(owner=request.user, term=CURRENT_TERM)
     if schedule.count() == 0:
         s = Schedule.objects.create(owner=request.user, term=CURRENT_TERM)
